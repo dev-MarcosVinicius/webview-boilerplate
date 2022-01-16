b3c92682ef40a5c8e2d5fb7048112a151d509c09
marcos.oliveira6 <marcosoliveira.ti9@gmail.com>
2022-01-16 14:19:19 -0300

    configurations of project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{ uri: 'https://reactnative.dev/' }} />
+      
     </View>
   );
 }
@@ -14,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+    
   },
 });
